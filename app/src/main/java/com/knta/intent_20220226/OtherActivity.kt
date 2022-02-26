1b9ebfb09e3e5b7a9add6f81ee5df78d78f9961b
skyoo1226 <you1226@naver.com>
2022-02-26 10:43:17 +0900

    enqjsWo ghkaus cnrk

---
+++ app/src/main/java/com/knta/intent_20220226/OtherActivity.kt
@@ -0,0 +1,11 @@
+package com.knta.intent_20220226
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+    }
+}
